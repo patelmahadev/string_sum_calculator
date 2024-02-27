8ec2bed4ab1ddba99414cbfdc199f2ef728df077
patelmahadev <mahadev.patel1227@gmail.com>
2024-02-27 11:24:45 +0530

    added test case if there is one input only

--- string_sum_calculator.rb
+++ string_sum_calculator.rb
@@ -3,13 +3,19 @@ require 'minitest/autorun'
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
+    return numbers.to_i if numbers.length == 1
   end
 end
 
 puts StringCalculator.add("") # => 0
+puts StringCalculator.add("1") # => 1
 
 class StringCalculatorTest < Minitest::Test
   def test_add_with_empty_string
     assert_equal 0, StringCalculator.add("")
+  end
+
+  def test_add_with_one_number
+    assert_equal 1, StringCalculator.add("1")
   end
 end
