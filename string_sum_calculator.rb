ee1564c19b28554bc166f0b0c77d3ee3ea4a2dcc
patelmahadev <mahadev.patel1227@gmail.com>
2024-02-27 11:52:10 +0530

    updated method to add two number

--- string_sum_calculator.rb
+++ string_sum_calculator.rb
@@ -3,12 +3,16 @@ require 'minitest/autorun'
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.length == 1
+
+    numbers = numbers.split(/,|\n/)
+    numbers.map(&:to_i).sum
+
   end
 end
 
 puts StringCalculator.add("") # => 0
 puts StringCalculator.add("1") # => 1
+puts StringCalculator.add("1,5") # => 6
 
 class StringCalculatorTest < Minitest::Test
   def test_add_with_empty_string
@@ -17,5 +21,9 @@ class StringCalculatorTest < Minitest::Test
 
   def test_add_with_one_number
     assert_equal 1, StringCalculator.add("1")
+  end
+
+  def test_add_with_two_numbers
+    assert_equal 3, StringCalculator.add("1,2")
   end
 end
